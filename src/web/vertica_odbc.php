3a06f336abc2d6a423713ceab29408c727b0bd85
Iara Ribeiro <Iara.prbr@gmail.com>
2013-07-31 14:57:06 -0300

    add files vm

--- src/web/vertica_odbc.php
+++ src/web/vertica_odbc.php
@@ -1,5 +1,7 @@
 <?php
 
+	$teste = "outro teste";
+	echo <b>"".$teste.""</b>;
     # Turn on error reporting
     error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
@@ -20,23 +22,23 @@
     echo "Connected with DSN: $dsn" . "\n";
    
     # Create a table
-    $sql = "CREATE TABLE TEST(
-            C_ID INT,
-            C_FP FLOAT,
-            C_VARCHAR VARCHAR(100),
-            C_DATE DATE,
-            C_TIME TIME,
-            C_TS TIMESTAMP)";
-    $result = errortrap_odbc($conn, $sql);
+//    $sql = "CREATE TABLE TEST(
+//            C_ID INT,
+//            C_FP FLOAT,
+//            C_VARCHAR VARCHAR(100),
+//            C_DATE DATE,
+//            C_TIME TIME,
+//            C_TS TIMESTAMP)";
+//    $result = errortrap_odbc($conn, $sql);
    
     # Insert data into the table with a standard SQL statement
 
-    $sql = "INSERT into test values(1,1.1,'abcdefg1234567890','1901-01-01','23:12:34 ','1901-01-01 09:00:09')";
-    $result = errortrap_odbc($conn, $sql);
+//    $sql = "INSERT into test values(1,1.1,'abcdefg1234567890','1901-01-01','23:12:34 ','1901-01-01 09:00:09')";
+//    $result = errortrap_odbc($conn, $sql);
    
     # Insert data into the table with odbc_prepare and odbc_execute
-    $values = array(2,2.28,'abcdefg1234567890','1901-01-01','23:12:34','1901-01-01 09:00:09');
-    $statement = odbc_prepare($conn,"INSERT into test values(?,?,?,?,?,?)");
+//    $values = array(2,2.28,'abcdefg1234567890','1901-01-01','23:12:34','1901-01-01 09:00:09');
+//    $statement = odbc_prepare($conn,"INSERT into test values(?,?,?,?,?,?)");
    
     if(!$result = odbc_execute($statement, $values)) {
         echo "odbc_execute Failed!" . "\n";
@@ -45,7 +47,7 @@
     }
    
     # Get the data from the table and display it
-    $sql = "SELECT * FROM TEST";
+    $sql = "SELECT * FROM EMPRESA WHERE cnpj = 01547749000116";
     if($result = errortrap_odbc($conn, $sql)) {
         while($row = odbc_fetch_array($result) ) {
             print_r($row);
