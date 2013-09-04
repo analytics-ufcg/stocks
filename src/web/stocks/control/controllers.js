f8ec1bc424f6e46c87ef3fd88671525ac5ac0425
Iara Ribeiro <eliaspaulinom@gmail.com>
2013-09-04 12:56:26 -0300

    atualizacao data
    Nao deixar adicionar data inicial maior que final
    Nao deixar usuario digita data

--- src/web/stocks/control/controllers.js
+++ src/web/stocks/control/controllers.js
@@ -31,17 +31,46 @@ function main_controller(){
 		run_top10();
 	});
 
-    $('#top10_datetimepicker1').datetimepicker({
-		language: 'pt-BR',
-	  	pickTime: false,
-	  	maskInput: true
+	var lowerBound = new Date(1950, 1, 1, 0, 0, 0, 0);
+	var now = new Date();
+	var upperBound = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
+
+	var start_field = $("#start_date_wrapper").datepicker({
+		onRender : function(date) {
+			return date.valueOf() <= lowerBound.valueOf() || date.valueOf() > upperBound.valueOf() ? 'disabled' : '';
+		}
+		}).on('changeDate', function(ev) {
+		if ( ev.date.valueOf() > end_field.date.valueOf() ){
+			var newDate = new Date(ev.date);
+			newDate.setDate(newDate.getDate());
+			end_field.setValue(newDate);
+		}
+	}).data('datepicker');
+
+	$("#start_date_wrapper").datepicker('setValue', new Date(now.getFullYear(), now.getMonth(), 1, 0, 0, 0, 0));
+	$("#start_date").click(function() {
+		$("start_date_wrapper").datepicker('show');
 	});
 
-	$('#top10_datetimepicker2').datetimepicker({
-		language: 'pt-BR',
-	  	pickTime: false,
-	  	maskInput: true
+
+
+	var end_field = $("#end_date_wrapper").datepicker({
+		onRender : function(date) {
+			return date.valueOf() <= start_field.date.valueOf() || date.valueOf() > upperBound.valueOf() ? 'disabled' : '';
+		}
+	}).on('changeDate', function(ev) {
+		if ( ev.date.valueOf() < start_field.date.valueOf() ){
+			var newDate = new Date(ev.date);
+			newDate.setDate(newDate.getDate());
+			start_field.setValue(newDate);
+		}
+	}).data('datepicker');
+
+	$("#end_date_wrapper").datepicker('setValue', upperBound);
+	$("#end_date").click(function() {
+		$("end_date_wrapper").datepicker('show');
 	});
+
 }
 
 /*
