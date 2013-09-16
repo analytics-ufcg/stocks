0c038301f10085634d38d29ac5b48cc1831785b9
Augusto Queiroz <augustoqmacedo@gmail.com>
2013-09-16 15:02:00 -0300

    Coleta Estadao para a Petrobras = Running

--- src/data_collection/coletor_noticias_estadao.php
+++ src/data_collection/coletor_noticias_estadao.php
@@ -14,9 +14,10 @@ print("============= Coletor de Links de Noticias do Estadão por Empresa ======
 # TODO: Read the EMPRESA queries from the .csv file and iteratively collect the
 # data by the query_strings
 
-# For instance we define a unique EMPRESA
+# For instance we define a unique EMPRESA: PETROBRAS and do not filter the interval
 // "query_string","query_start_date","query_end_date","nome_empresa","nome_pregao","cnpj","setor","sub_setor","segmento"
 // "petroleo brasileiro petrobras",1995-03-27,2013-07-05,"PETROLEO BRASILEIRO S.A. PETROBRAS","PETROBRAS","33000167000101","Petroleo. Gas e Biocombustiveis","Petroleo. Gas e Biocombustiveis","Exploracao e/ou Refino"
+$nome_pregao = "PETROBRAS";
 $cnpj = '33000167000101';
 $query_string = 'petrobras';
 
@@ -57,11 +58,11 @@ do{
 
 	for ($i = 0; $i < count($all_links_data); $i++){
 		# Date
-		$links_array[1] = read_text_between($all_links_data[$i], '<p class="listaNoticias_data">', '</p>');
+		$links_array[3] = read_text_between($all_links_data[$i], '<p class="listaNoticias_data">', '</p>');
 		# Title
-		$links_array[2] = read_text_between($all_links_data[$i], 'class="listaNoticias_titulo" title="', '">');
+		$links_array[4] = read_text_between($all_links_data[$i], 'class="listaNoticias_titulo" title="', '">');
 		# Link
-		$links_array[3] = read_text_between($all_links_data[$i], '<a href="', '">');
+		$links_array[5] = read_text_between($all_links_data[$i], '<a href="', '">');
 
 		# STORE THEM IN THE links_[empresa] CSV FILE
 		fputcsv($links_emp_csv_file, $links_array, ',', '"');
