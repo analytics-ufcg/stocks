f526bb221cb2d5ee3dc86269e84d84837457a8c9
Augusto Queiroz <augustoqmacedo@gmail.com>
2013-09-16 14:52:10 -0300

    Coleta Estadao para a Petrobras = Running

--- src/data_collection/coletor_noticias_estadao.php
+++ src/data_collection/coletor_noticias_estadao.php
@@ -1,16 +1,6 @@
 <?php
 
-# Create the COMMON data directories
-$news_dir = 'data/news';
-$estadao_dir = "$news_dir/estadao";
-
-if (!file_exists($news_dir)) {
-	mkdir($news_dir, 0777, true);
-}
-
-if (!file_exists($estadao_dir)) {
-	mkdir($estadao_dir, 0777, true);
-}
+include 'coletor_noticias_aux.php';
 
 # Define the ESTADAO search URL
 $search_url = 'http://economia.estadao.com.br/busca/';
@@ -19,103 +9,106 @@ $search_url = 'http://economia.estadao.com.br/busca/';
 # QUERY by EMPRESA
 # =============================================================================
 
+print("============= Coletor de Links de Noticias do Estadão por Empresa =============\n\n");
+
 # TODO: Read the EMPRESA queries from the .csv file and iteratively collect the
 # data by the query_strings
 
 # For instance we define a unique EMPRESA
-$nome_empresa = 'petrobras';
-$search_query = 'petrobras';
+// "query_string","query_start_date","query_end_date","nome_empresa","nome_pregao","cnpj","setor","sub_setor","segmento"
+// "petroleo brasileiro petrobras",1995-03-27,2013-07-05,"PETROLEO BRASILEIRO S.A. PETROBRAS","PETROBRAS","33000167000101","Petroleo. Gas e Biocombustiveis","Petroleo. Gas e Biocombustiveis","Exploracao e/ou Refino"
+$cnpj = '33000167000101';
+$query_string = 'petrobras';
+
+printf("Empresa: %s (busca: %s)\n", $nome_pregao, $query_string);
 
 # -----------------------------------------------------------------------------
 # LINKS RETRIEVAL
 # -----------------------------------------------------------------------------
 
+// print("---------------- Coleta de Links ----------------\n");
+
 # Create the empresa links file
-$links_emp_csv_filename = "$estadao_dir/links_" . str_replace(' ', '_', $nome_empresa) . ".csv";
+$links_emp_csv_filename = "$estadao_dir/links_" . str_replace(' ', '_', strtolower($nome_pregao)) . ".csv";
 $links_emp_csv_file = fopen($links_emp_csv_filename, "w");
 
-# CSV Header: Fonte, Caderno, Data, Titulo, Link
-$links_array = array('Estadao', 'NA', 'NA', 'NA', 'NA');
+# CSV Header: Fonte, Sub-Fonte, CNPJ, Data, Titulo, Link
+$links_array = array('Estadão.com.br', 'Economia & Negócios', $cnpj, 'NA', 'NA', 'NA');
 
-$emp_search_url = str_replace(" ", "%20", "$search_url$search_query");
+$emp_search_url = str_replace(" ", "%20", "$search_url$query_string");
 
-# TODO: Change this!!!!!!!!!!!!!!
-// $html_content = file_get_contents($emp_search_url);
-$html_content = file_get_contents('./file.txt');
+$html_content = file_get_contents($emp_search_url);
+// print_r($html_content);
+// $html_content = file_get_contents('./file.txt');
 
-# TODO: Get Caderno!
-
-# SELECT THE DESIRED PART OF THE PAGE
-$html_content = read_text_between($html_content, '<div class="topoPagBusca">', '<div class="c3">');
-
-# GET THE NUMBER OF PAGES RETURNED
+# Get the number of links
 $number_links = read_text_between($html_content, 'encontrados <em>', '</em> registros para');
+printf("  Total de Links: %d\n", $number_links);
 
-echo("There are $number_links links.\n");
+$count_links_retrieved = 0;
+do{
+	# Select the desired part of the page
+	$html_content = read_text_between($html_content, '<div class="topoPagBusca">', '<div class="c3">');
 
-# GET ALL: NEWS_TITLE, DATE AND LINK
-// preg_match_all(pattern, subject, matches)
-// <p class="listaNoticias_data">13/09/2013</p>
-// <a href="http://economia.estadao.com.br/noticias/negocios-geral,petrobras-aprova-venda-na-colombia-por-us-380-mi,164711,0.htm">
-// <h2 class="listaNoticias_titulo" title="Petrobras aprova venda na Colômbia por US$ 380 mi">Petrobras aprova venda na Colômbia por US$ 380 mi</h2></a>
-// <a href="http://economia.estadao.com.br/noticias/negocios-geral,petrobras-aprova-venda-na-colombia-por-us-380-mi,164711,0.htm" class="listaNoticias_chamada">
+	# Match the html text with the: NEWS_TITLE, DATE AND LINK
+	$all_links_data = match_all_between($html_content, '<p class="listaNoticias_data">', 'class="listaNoticias_chamada">');
+	# Select the complete match (with leading and trailing strings)
+	$all_links_data = $all_links_data[0]; 
 
-$all_links_data = match_all_between($html_content, '<p class="listaNoticias_data">', 'class="listaNoticias_chamada">');
-$all_links_data = $all_links_data[0]; # Select the complete match (with leading and trailing strings)
+	for ($i = 0; $i < count($all_links_data); $i++){
+		# Date
+		$links_array[1] = read_text_between($all_links_data[$i], '<p class="listaNoticias_data">', '</p>');
+		# Title
+		$links_array[2] = read_text_between($all_links_data[$i], 'class="listaNoticias_titulo" title="', '">');
+		# Link
+		$links_array[3] = read_text_between($all_links_data[$i], '<a href="', '">');
 
-for ($i = 0; $i < count($all_links_data); $i++){
-	$date = read_text_between($all_links_data[$i], '<p class="listaNoticias_data">', '</p>');
-	$link = read_text_between($all_links_data[$i], '<a href="', '">');
-	$title = read_text_between($all_links_data[$i], 'class="listaNoticias_titulo" title="', '">');
+		# STORE THEM IN THE links_[empresa] CSV FILE
+		fputcsv($links_emp_csv_file, $links_array, ',', '"');
 
-	$links_array[2] = $date;
-	$links_array[3] = $title;
-	$links_array[4] = $link;
+	}
+	# Increment the retrieved links counter
+	$count_links_retrieved += count($all_links_data);
+
+	printf("    Links coletados: %d/%d\n", $count_links_retrieved, $number_links);
 	
-	# STORE THEM IN THE links_[empresa] CSV FILE
-	fputcsv($links_emp_csv_file, $links_array, ',', '"');
-}
+	if ($count_links_retrieved < $number_links){
+		# Wait some seconds (from 1 to 5 secs randomly)
+		sleep(rand(1, 3));
 
-# CHECK IF THERE IS MORE LINKS TO RETRIEVE
+		# Get the html file of the next page (only adds a /2 in the end of the URL)
+		$html_content = file_get_contents($emp_search_url . '/2');
+	}else{
+		break;
+	}
 
-# CLOSE THE LINKS CSV FILE
+}while (true);
+
+# Close the links CSV file
 fclose($links_emp_csv_file);
 
 # -----------------------------------------------------------------------------
 # TEXT RETRIEVAL
 # -----------------------------------------------------------------------------
 
-# Create the empresa text data file
-$text_emp_csv_file = fopen("$estadao_dir/text_" . str_replace(' ', '_', $nome_empresa) . ".csv", "w");
+// print("---------------- Coleta de Texto ----------------\n");
 
-# CSV Header: Link, Data, Titulo, Corpo
-$text_array = array('NA', 'NA', 'NA', 'NA');
+// # Create the empresa text data file
+// $text_emp_csv_file = fopen("$estadao_dir/text_" . str_replace(' ', '_', $nome_pregao) . ".csv", "w");
 
-# TODO
-# READ THE link_[empresa] CSV FILE
-# RETRIEVE THE TITLE, DATA AND BODY
-# STORE THEM IN THE data_[empresa] CSV FILE
+// # CSV Header: Fonte, Caderno, Data, Titulo, Link, Corpo (ALL together in the same file)
+// $text_array = array('Estadão', 'Economia', 'NA', 'NA', 'NA', 'NA');
 
-fputcsv($text_emp_csv_file, $text_array, ',', '"');
+// # TODO
+// # READ THE link_[empresa] CSV FILE
+// # RETRIEVE THE CADERNO
+// # RETRIEVE THE BODY
+// // read_text_between($html_content, '<div class="corpo">', '<!--Template: economia-->');
 
-fclose($text_emp_csv_file);
+// # STORE all columns IN THE data_[empresa] CSV FILE
 
-function read_text_between($text, $start_tag, $end_tag){
-	$starts_at = strpos($text, $start_tag) + strlen($start_tag);
-	$ends_at = strpos($text, $end_tag, $starts_at);
-	return substr($text, $starts_at, $ends_at - $starts_at);
-}
+// fputcsv($text_emp_csv_file, $text_array, ',', '"');
 
-function match_all_between($text, $start_tag, $end_tag){
-	$delimiter = '#';
-	$regex = $delimiter . preg_quote($start_tag, $delimiter) 
-	                    . '(.*?)' 
-	                    . preg_quote($end_tag, $delimiter) 
-	                    . $delimiter 
-	                    . 's';
-	preg_match_all($regex, $text, $result_array);
-	return $result_array;
-}
+// fclose($text_emp_csv_file);
 
-
-// ?>
+?>
