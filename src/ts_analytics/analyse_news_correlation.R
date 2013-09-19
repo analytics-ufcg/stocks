cedb0ae488a62b3d8b9a307f655238d1823e1ea9
Danilo <dnlgomes93@gmail.com>
2013-09-19 16:49:25 -0300

    grafico da petrobras

--- src/ts_analytics/analyse_news_correlation.R
+++ src/ts_analytics/analyse_news_correlation.R
@@ -1,12 +1,15 @@
+rm(list = ls())
+
 library(RODBC)
+library(plyr)
+library(zoo)
 
 myconn <- odbcConnect("StocksDSN")
 
-news.count.query <- 
-"SELECT cnpj, data_noticia, fonte, count(*) AS num_noticias
-FROM link_noticias_empresa
-GROUP BY cnpj, fonte, data_noticia
-ORDER BY data_noticia ASC"
+news.count.query <- "SELECT cnpj, data_noticia, fonte, count(*) AS num_noticias
+                     FROM link_noticias_empresa
+                     GROUP BY cnpj, fonte, data_noticia
+                     ORDER BY data_noticia ASC"
 
 news.count.df <- sqlQuery(myconn, news.count.query)
 
@@ -22,14 +25,38 @@ emp.ts.df <- sqlQuery(myconn, emp.ts.query)
 
 close(myconn)
 
-# TODO:
-# For each fonte:
-#   Interpolate the NUM_NOTICIAS with zeros (0)
- 
-# For each CNPJ:
-#   For each ISIN:
-#     Interpolate the COTACAO with constant values
 
+# For each (FONTE and CNPJ):
+#   Interpolate the NUM_NOTICIAS with zeros (0)
+news.count.filled <- ddply(news.count.df, .(fonte, cnpj), function(df){
+  
+  news.ts <- zoo(df$num_noticias, df$data_noticia)
+  news.ts.complete <- merge.zoo(news.ts,
+                               zoo(, seq(start(news.ts), end(news.ts), by="day")), all=TRUE)
+  news.ts.complete[is.na(news.ts.complete)] <- 0
+  return(data.frame(data_noticia = index(news.ts.complete), 
+                    num_noticias = as.vector(news.ts.complete)))
+}, .progress = "text")
+
+# For each (CNPJ and ISIN):
+#   Interpolate the COTACAO with constant values
+emp.ts.filled <- ddply(emp.ts.df, .(cnpj, cod_isin), function(df){
+  
+  emp.ts <- zoo(df$preco_ultimo, df$data_pregao)
+  emp.ts.complete <- merge.zoo(emp.ts,
+                               zoo(, seq(start(emp.ts), end(emp.ts), by="day")), all=TRUE)
+  
+  # Fill the created gaps with the last non-NA value
+  emp.ts.complete <- na.locf(emp.ts.complete)
+  
+  return(data.frame(data_pregao = index(emp.ts.complete), 
+                    preco_ultimo = as.vector(emp.ts.complete)))
+}, .progress = "text")
+
+
+
+
+# TODO:
 # For each CNPJ:
 #   For each ISIN:
 #     For each fonte:
--- src/web/stocks/model/model_graficos.php
+++ src/web/stocks/model/model_graficos.php
@@ -6,7 +6,7 @@
     // $column = "setor";
 
     # Prepare the query
-   	$query = "select preco_medio, data_pregao from Cotacao where cod_isin = 'BRPETRACNOR9' and COD_BDI = '02'";
+   	$query = "select preco_medio, data_pregao from Cotacao where cod_isin = 'BRPETRACNOR9' and COD_BDI = '02' order by data_pregao asc";
     
     # Turn on error reporting
     error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
@@ -22,16 +22,21 @@
 
     # Fetch all rows
     $name_list = array();
-
+    $list_response = array();
     $data = array();
     $valores = array();
     while ($row = odbc_fetch_array($resultset)) {
+        array_push($list_response, array(strtotime($row['data_pregao'])*1000, (float)$row['preco_medio']));
         array_push($data, $row['data_pregao']);
         array_push($valores, $row['preco_medio']);
+        
     }
-
+    #arrayD = array($data, $valores);
 	# Close the connection
 	odbc_close($conn);
-
-    echo json_encode(array("data" => $data, "valores" => $valores));
+//echo json_encode(array("data" => $data, "valores" => $valores)); array(["1158278400000",74.10],["1158278400000",90.10])
+    // echo json_encode(array([1158270400000,74.10],[1158271400000,90.10], [1158272400000,74.10], [1158277400000,74.10]));
+  //  echo json_encode(array([$data,$valores]));
+    //print_r($list_response);
+    echo json_encode($list_response);
 ?>
