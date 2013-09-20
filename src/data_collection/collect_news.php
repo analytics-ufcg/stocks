9e9ae378a6e78be868b2a07ffabb1ebe15a658a5
Augusto QM <augustoqmacedo@gmail.com>
2013-09-19 23:29:01 -0300

    Adequando a coleta da folha

--- src/data_collection/collect_news.php
+++ src/data_collection/collect_news.php
@@ -2,14 +2,11 @@
 
 include 'collect_news_aux.php';
 
-# Define the ESTADAO search URL
-$search_url = 'http://economia.estadao.com.br/busca/';
-
 # =============================================================================
 # QUERY by EMPRESA
 # =============================================================================
 
-print("============= Coletor de Links de Noticias do Estadão por Empresa =============\n\n");
+print("============= Coletor de Links de Noticias por Empresa =============\n\n");
 
 # TODO then: Read the EMPRESA queries from the .csv file and iteratively collect the
 # data by the query_strings
@@ -21,19 +18,18 @@ $nome_pregao = "PETROBRAS";
 $cnpj = '33000167000101';
 $query_string = 'petrobras';
 
-printf("Empresa: %s (busca: %s)\n", $nome_pregao, $query_string);
+printf("Empresa: %s (busca: %s)\n\n", $nome_pregao, $query_string);
 
 # -----------------------------------------------------------------------------
 # LINKS RETRIEVAL
 # -----------------------------------------------------------------------------
 
-# Define the complete query string
-$emp_search_url = str_replace(" ", "%20", "$search_url$query_string");
-
 # Run the Collection to the Estadão.com.br
-collect_estadao($news_dir, $nome_pregao, $cnpj, $emp_search_url);
+// printf("===== Estadão.com.br =====\n\n");
+// collect_estadao($news_dir, $nome_pregao, $cnpj, $query_string);
 
-# Run the Collection to the Estadão.com.br
-collect_folha_sao_paulo($news_dir, $nome_pregao, $cnpj, $emp_search_url);
+# Run the Collection to the Folha de São Paulo
+printf("===== Folha de São Paulo =====\n\n");
+collect_folha_sao_paulo($news_dir, $nome_pregao, $cnpj, $query_string);
 
 ?>
