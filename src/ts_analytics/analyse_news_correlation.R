b6d8e2a4f3a890c96d0af8f716f5414f8faabbf6
Augusto QM <augustoqmacedo@gmail.com>
2013-09-21 13:41:10 -0300

    News Correlation Analysis = almost DONE (small changes to finish it all)

--- src/ts_analytics/analyse_news_correlation.R
+++ src/ts_analytics/analyse_news_correlation.R
@@ -32,9 +32,9 @@ PlotTsWithSolavanco <- function(serie, solavanco, ylab, main.title){
 # myconn <- odbcConnect("StocksDSN")
 # 
 # cat("Selecting the NEWS COUNT per (DAY and CNPJ)...\n")
-# news.count.query <- "SELECT cnpj, data_noticia, fonte, count(*) AS num_noticias
+# news.count.query <- "SELECT cnpj, data_noticia, count(*) AS num_noticias
 #                      FROM link_noticias_empresa
-#                      GROUP BY cnpj, fonte, data_noticia
+#                      GROUP BY cnpj, data_noticia
 #                      ORDER BY data_noticia ASC"
 # 
 # news.count.df <- sqlQuery(myconn, news.count.query)
@@ -52,10 +52,10 @@ PlotTsWithSolavanco <- function(serie, solavanco, ylab, main.title){
 # cat("Closing the connection.\n")
 # close(myconn)
 
-# For each (FONTE and CNPJ):
+# For each (CNPJ):
 #   Interpolate the NUM_NOTICIAS with zeros (0)
 cat("Interpolating with zeros the days without NEWS...\n")
-news.count.filled <- ddply(news.count.df, .(fonte, cnpj), function(df){
+news.count.filled <- ddply(news.count.df, .(cnpj), function(df){
   
   news.ts <- zoo(df$num_noticias, df$data_noticia)
   news.ts.complete <- merge.zoo(news.ts,
@@ -81,20 +81,18 @@ emp.ts.filled <- ddply(emp.ts.df, .(cnpj, cod_isin), function(df){
                     preco_ultimo = as.vector(emp.ts.complete)))
 }, .progress = "text")
 
-# For each Fonte:
-#   For each (CNPJ and ISIN):
-#       Select the intersection between the series
-#       Calculate the Pearson Correlation (for the hole series)
-#       Calculate the Pearson Correlation (for the days with noticias only AND filled Cotacao)
-#       Calculate the Pearson Correlation (for the days with Cotacao only AND filled news)
-#       Calculate the Pearson Correlation (for the days with Cotacao only AND news)
-#       Select the SOLAVANCOs
-#       Calculate the Pearson Correlation for the Solavancos
-#       Plot the Price and NewsCount Time-Series with the solavancos highlighted
+# For each (CNPJ and ISIN):
+#     Select the intersection between the series
+#     Calculate the Pearson Correlation (for the hole series)
+#     Calculate the Pearson Correlation (for the days with noticias only AND filled Cotacao)
+#     Calculate the Pearson Correlation (for the days with Cotacao only AND filled news)
+#     Calculate the Pearson Correlation (for the days with Cotacao only AND news)
+#     Select the SOLAVANCOs
+#     Calculate the Pearson Correlation for the Solavancos
+#     Plot the Price and NewsCount Time-Series with the solavancos highlighted
 cat("Calculating the correlation between the (interpolated) NEWS COUNT and the (interpolated) COTACAO (per CNPJ and ISIN)...\n")
 cat("Plotting the Price and NewsCount Time-Series with the solavancos highlighted...\n")
 
-all.fontes <- as.character(unique(news.count.df$fonte))
 window.size <- 15
 emp.news.corr <- NULL
 output.dir <- "data/time_series/news_correlation"
@@ -104,7 +102,6 @@ pdf(paste(output.dir, "/news_correlation_ts.pdf", sep = ""), width = 25, height 
 
 d_ply(emp.ts.filled, .(cnpj, cod_isin), function(df){
   
-  for (this.fonte in all.fontes){
     ts.cnpj <- df$cnpj[1]
     ts.nome.pregao <- df$cnpj[1] #df$nome_pregao[1]
     ts.cod_isin <- as.character(df$cod_isin[1])
@@ -116,10 +113,10 @@ d_ply(emp.ts.filled, .(cnpj, cod_isin), function(df){
     price.ts.filled <- zoo(df$preco_ultimo, df$data_pregao)
     
     # Get BOTH, the Filled News and the News, TSs by CNPJ
-    news.df <- subset(news.count.df, fonte == this.fonte & cnpj == ts.cnpj)
+    news.df <- subset(news.count.df, cnpj == ts.cnpj)
     news.ts <- zoo(news.df$num_noticias, news.df$data_noticia)
     
-    news.df.filled <- subset(news.count.filled, fonte == this.fonte & cnpj == ts.cnpj)
+    news.df.filled <- subset(news.count.filled, cnpj == ts.cnpj)
     news.ts.filled <- zoo(news.df.filled$num_noticias, news.df.filled$data_noticia)
 
     rm(price.ts.tmp, news.df, news.df.filled)
@@ -157,9 +154,9 @@ d_ply(emp.ts.filled, .(cnpj, cod_isin), function(df){
     
     PlotTsWithSolavanco(intersect.price.ts, solavancos, 
                         ylab = "Preco_Ultimo",
-                        main.title = paste("Time-Series with Solavancos\n", ts.nome.pregao, 
-                                           " - ", ts.cod_isin,"    vs.    ", trim(this.fonte), 
-                                           "\nPearson Correlations: plain= ", 
+                        main.title = paste("Time-Series with Solavancos (", ts.nome.pregao, 
+                                           " - ", ts.cod_isin, 
+                                           ")\nPearson Correlations: plain= ", 
                                            round(ts.correlation.filled, 3), 
                                            " / solavanco weighted= ", 
                                            round(solavanco.weighted.corr, 3), 
@@ -171,10 +168,9 @@ d_ply(emp.ts.filled, .(cnpj, cod_isin), function(df){
     PlotTsWithSolavanco(intersect.news.ts, solavancos, 
                         ylab = "Number of News", main.title = "")
     
-  }
+    #   }
 }, .progress = "text")
 dev.off()
 
 # TODO: Fazer a interpolação dos preços no Vertica
-# TODO: Unir a contagem de notícias por dia (independentemente de fontes)
 # TODO: Add o nome_pregao nos plots (mudar consulta)
