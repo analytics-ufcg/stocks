21925c660e904dc79fe68ea174866501a2447cde
Augusto QM <augustoqmacedo@gmail.com>
2013-09-24 02:30:00 -0300

    Coleta Folha - Refatorada

--- src/data_collection/collect_news_aux.php
+++ src/data_collection/collect_news_aux.php
@@ -51,7 +51,7 @@ function collect_estadao($news_dir, $nome_pregao, $cnpj, $query_string){
 		# Select the desired part of the page
 		$html_content = read_text_between($html_content, '<div class="topoPagBusca">', '<div class="c3">');
 
-		# Match the html text with the: NEWS_TITLE, DATE AND LINK
+		# Match all the html text with the: NEWS_TITLE, DATE AND LINK
 		$all_links_data = match_all_between($html_content, '<p class="listaNoticias_data">', 'class="listaNoticias_chamada">');
 		# Select the complete match (with leading and trailing strings)
 		$all_links_data = $all_links_data[0]; 
@@ -97,46 +97,6 @@ function collect_estadao($news_dir, $nome_pregao, $cnpj, $query_string){
 
 function collect_folha_sao_paulo($news_dir, $nome_pregao, $cnpj, $query_string){
 
-	function get_between($input, $start, $end) 
-	{ 
-		$list = array();
-		while(strpos($input, $start) !== false && strpos($input, $end) !== false){
-			$substr = substr($input, strlen($start)+strpos($input, $start), (strlen($input) - strpos($input, $end))*(-1));
-			$input = substr($input, (strlen($end) + strpos($input, $end)),strlen($input)); 
-			array_push($list, $substr);
-
-		}
-
-		return $list;
-	} 
-
-	function get_result_set($number_page,$complemento_url){
-		$page_news_url = "http://search.folha.com.br/search?q=".$complemento_url."&sr=".$number_page;
-		$html_page = file_get_contents($page_news_url);
-		$result = get_between($html_page,'<!--RESULTSET-->','<!--/RESULTSET-->');
-		
-		return $result[0];
-		
-	}
-
-	function get_number_pages($result_set){
-		$pages_number = get_between($result_set,'resultados <span>(',')</span>');
-		$pages_number = preg_split('/de/',$pages_number[0]);
-		
-		return $pages_number[1];
-	}
-
-	function get_list_links_result_set($result_set){
-		$list_results = get_between($result_set,'<span class="url">','</span>');
-		return $list_results;
-	}
-
-	function get_list_string_with_link_title_date($result_set){
-		$result = get_between($result_set,'<a href="',"</a><br>");
-		return $result;
-	}
-
-
 	# Create the empresa links file
 	$links_emp_csv_filename = "$news_dir/links_folha_" . str_replace(' ', '_', strtolower($nome_pregao)) . ".csv";
 
@@ -144,80 +104,131 @@ function collect_folha_sao_paulo($news_dir, $nome_pregao, $cnpj, $query_string){
 	$links_emp_csv_file = fopen($links_emp_csv_filename, "w");
 
 	# CSV Header: Fonte, Sub-Fonte, CNPJ, Data, Titulo, Link
-	$links_array = array('Folha de S.Paulo', 'Mercado e Dinheiro', $cnpj, 'NA', 'NA', 'NA');
+	$links_row = array('Folha de S.Paulo', 'Mercado e Dinheiro', $cnpj, 'NA', 'NA', 'NA');
 
+	$initial_date = '2002-01-01';
+	$final_date = date( "Y-m-d"); # TODAY
+
+	$month_steps = 3;
+
+	$curr_start_date = $initial_date;
+	$curr_end_date = $initial_date;
 	
-	$year_list = range(2002, 2013);
-	$month_list = range(01, 12);
+	$total_links = 0;	
 
-	foreach ($year_list as $year) {
-		for($month=0; $month < count($month_list) - 1; $month++) {
-			
-			$complemento_url = $query_string."&site=online%2Fdinheiro&sd=02%2F".$month_list[$month]
-			."%2F".$year."&ed=01%2F".$month_list[$month + 1]."%2F".$year;
-
-			print("DATA CONSULTA: 02/".$month_list[$month]."/".$year." - 01/".$month_list[$month + 1]."/".$year);
-			
-			# Create the empresa links file
-
-			$result_set = get_result_set(1,$complemento_url);
-			
-			$list_links = get_list_string_with_link_title_date($result_set);
-			
-			$number_links = get_number_pages($result_set);
-			$number_links = (int) str_replace(".", "", $number_links);
-			
-			printf("  Total de Links: %d\n", $number_links);
-			//valido os indices de 1 a 3
-
-			for($i=0;$i < count($list_links);$i++){
-
-				$hifen_position = strrpos($list_links[$i], " - ");
-				$date = substr($list_links[$i],$hifen_position + 3, $hifen_position + 8);
-				list ($this_day, $this_month, $this_year) = split("/", $date);
-				
-				$link_title = preg_split('/">/', substr($list_links[$i], 0, $hifen_position));
-				$link = $link_title[0];
-				$title = str_replace('Folha.com - ', '', str_replace('Folha Online - ', '', str_replace('"', '\"', $link_title[1])));
-
-				//valido indice 1
-				$links_array[3] =  "$this_year-$this_month-$this_day";
-				$links_array[4] =  $title;
-				$links_array[5] =  $link;
-				fputcsv($links_emp_csv_file, $links_array, ',', '"');			
-			}
-
-			$iterator_after_first_page = 26;
-			while($iterator_after_first_page <= $number_links){
-
-				printf("    Links coletados: %d/%d\n", $iterator_after_first_page, $number_links);
-				
-				$result_set = get_result_set($iterator_after_first_page,$complemento_url);
-				$list_links = get_list_string_with_link_title_date($result_set);
-				
-
-				//valido os indices de 1 a 3
-				for($i=0;$i < count($list_links);$i++){
-					
-					$hifen_position = strrpos($list_links[$i], " - ");
-					$date = substr($list_links[$i], $hifen_position + 3, $hifen_position + 8);
-					list ($this_day, $this_month, $this_year) = split("/", $date);
-					$link_title = preg_split('/">/', substr($list_links[$i], 0,$hifen_position));
-					$links_array[3] =  "$this_year-$this_month-$this_day";
-					$link = $link_title[0];
-					$title = str_replace('Folha.com - ', '', str_replace('Folha Online - ', '', str_replace('"', '\"', $link_title[1])));
-
-					//valido indice 1
-					$links_array[4] =  $title;
-					$links_array[5] =  $link;
-					fputcsv($links_emp_csv_file, $links_array, ',', '"');
-					
-				}
-				$iterator_after_first_page +=  25;
-				sleep(rand(1, 2));
-			}
+	while(true){
+		if($curr_end_date >= $final_date){
+			break;
 		}
+
+		$next_date = date( "Y-m-d", strtotime("$curr_start_date +$month_steps month"));
+		if ($next_date >= $final_date){
+			$curr_end_date = $final_date; # Last iteration
+		}else {
+			$curr_end_date = $next_date; # Update to the next 3 months
+		}
+		
+		// echo $curr_start_date, "  ", $curr_end_date, "\n";
+		list($year, $month, $day) = split('-', $curr_start_date);
+		$start_date = "$day-$month-$year";
+		list($year, $month, $day) = split('-', $curr_end_date);
+		$end_date = "$day-$month-$year";
+
+		print("Date Interval: $start_date - $end_date\n");
+				  			
+		# Create the empresa links file
+		$full_url = "http://search.folha.com.br/search?q=" . $query_string . 
+					"&site=online%2Fdinheiro&sd=" . str_replace('-', '%2F', $start_date) .
+					'&ed=' . str_replace('-', '%2F', $end_date);
+		// echo $full_url, "\n" ;
+
+		$html_content = file_get_contents($full_url);
+		// $html_content = file_get_contents('file.txt');
+
+		$html_content = read_text_between($html_content, '<!--RESULTSET-->', '<!--/RESULTSET-->');
+		// echo $html_content;
+
+		# Get the number of links
+		if (strstr($html_content, 'Nenhum resultado de busca encontrado para a expressão') === true){
+			echo "No link found!";
+			continue;
+		}
+
+		$number_links = split(' de ', read_text_between($html_content, 
+														'<h2 class="localSearchTarja">resultados <span>(', ')</span></h2>'));
+		$number_links = $number_links[1];
+		printf("  Number of Links: %d\n", $number_links);
+		
+		$count_links_retrieved = 0;
+		
+		do{
+
+			# Match all the html text with the: NEWS_TITLE, DATE AND LINK
+			$all_links_data = match_all_between($html_content, '<a href="','</a><br>');
+			# Select the complete match (with leading and trailing strings)
+			$all_links_data = $all_links_data[0]; 
+			// print_r($all_links_data);
+			
+			for ($i = 0; $i < count($all_links_data); $i++){
+
+				$hyphen = ' - ';
+
+				# Date
+				$last_hyphen = strrpos($all_links_data[$i], $hyphen);
+				$date_news = str_replace('</a><br>', '', substr($all_links_data[$i], $last_hyphen + strlen($hyphen)));
+				list ($day, $month, $year) = split("/", $date_news);
+				$links_row[3] =  "$year-$month-$day";
+				// echo $links_row[3], "\n";
+
+				# Title
+				$first_hyphen = strpos($all_links_data[$i], $hyphen);
+				$second_hyphen = strpos($all_links_data[$i], $hyphen, $first_hyphen + strlen($hyphen));
+				$title_news = substr($all_links_data[$i], $second_hyphen + strlen($hyphen));
+				$last_hyphen_title = strrpos($title_news, $hyphen);
+				$title_news = substr($title_news, 0, (strlen($title_news) - $last_hyphen_title) * -1);
+				$links_row[4] = str_replace('"', '\"', $title_news);
+				// echo $links_row[4], "\n";
+
+				# Link
+				$links_row[5] = read_text_between($all_links_data[$i], '<a href="', '">');
+				// echo $links_row[5], "\n";
+
+				# Sub-Fonte
+				$sub_fonte = read_text_between($all_links_data[$i], $hyphen, $hyphen);
+				$links_row[1] = $sub_fonte;
+				// echo $links_row[1], "\n\n";
+
+				# STORE THEM IN THE links_[empresa] CSV FILE
+				fputcsv($links_emp_csv_file, $links_row, ',', '"');
+			}
+
+
+			# Increment the retrieved links counter
+			$count_links_retrieved += count($all_links_data);
+			
+			printf("    Links coletados: %d/%d\n", $count_links_retrieved, $number_links);
+			
+			if ($count_links_retrieved < $number_links){
+				# Wait some seconds (from 1 to 5 secs randomly)
+				sleep(rand(1, 3));
+
+				# Get the html file of the next page (only add 1 to the links retrieved)				
+				$html_content = file_get_contents($full_url . '&sr=' . ($count_links_retrieved + 1));
+				# Select the desired part of the page
+				$html_content = read_text_between($html_content, '<!--RESULTSET-->', '<!--/RESULTSET-->');
+			}else{
+				break;
+			}
+
+		}while(true);
+		
+		$total_links += $number_links;
+		echo $total_links, "\n";
+		$curr_start_date = date( "Y-m-d", strtotime("$curr_end_date +1 day"));
+		
 	}
+	printf("Total of Retrieved Links: %d\n", $total_links);
+
 	fclose($links_emp_csv_file);
 
 }
