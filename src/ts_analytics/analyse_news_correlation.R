6f6ed7f3b5150d8747b8859c8c7ab72f59399ac6
Augusto Queiroz <augustoqmacedo@gmail.com>
2013-10-03 17:47:54 -0300

    see news correlation

--- src/ts_analytics/analyse_news_correlation.R
+++ src/ts_analytics/analyse_news_correlation.R
@@ -39,9 +39,13 @@ news.count.query <- "SELECT cnpj, fonte, data_noticia, count(*) AS num_noticias
 news.count.df <- sqlQuery(myconn, news.count.query)
 news.count.df <- news.count.df[-c(1:2),]
 
-cat("Selecting all COTACAO (already interpolated) with the CNPJs that have at least one NOTICIA assigned to it ...\n")
+cat("Selecting all COTACAO (already interpolated) with the CNPJs that have: at least one day with cotacao AND one NOTICIA assigned to it ...\n")
 emp.ts.query <- "SELECT emp.cnpj, emp.nome_pregao, emp_isin.cod_isin, acao.data_pregao, acao.preco_ultimo
-                FROM empresa AS emp INNER JOIN empresa_isin AS emp_isin ON emp.cnpj = emp_isin.cnpj
+                FROM empresa AS emp INNER JOIN (
+                            SELECT cnpj, cod_isin
+                            FROM empresa_isin
+                            WHERE tamanho_cotacao > 0
+                      ) AS emp_isin ON emp.cnpj = emp_isin.cnpj
                      INNER JOIN (
                   		     SELECT slice_time as data_pregao, cod_isin, 
                 				         TS_FIRST_VALUE(preco_ultimo IGNORE NULLS, 'const') as preco_ultimo
@@ -104,38 +108,33 @@ d_ply(emp.ts.filled, .(cnpj, cod_isin), function(df){
       ts.nome.pregao <- df$nome_pregao[1]
       ts.cod_isin <- as.character(df$cod_isin[1])
         
-      # Get BOTH, the Filled Price and the Price, TSs by the CNPJ and ISIN
-      price.ts.tmp <- subset(emp.ts.filled, cnpj == ts.cnpj & cod_isin == ts.cod_isin)
-      price.ts <- zoo(price.ts.tmp$preco_ultimo, price.ts.tmp$data_pregao)
-  
+      # Create the zoo object
       price.ts.filled <- zoo(df$preco_ultimo, df$data_pregao)
       
-      # Get BOTH, the Filled News and the News, TSs by CNPJ
-      news.df <- subset(news.count.df, fonte == this.fonte & cnpj == ts.cnpj)
-      news.ts <- zoo(news.df$num_noticias, news.df$data_noticia)
-      
+      # Create the Filled News TS object by FONTE and CNPJ
       news.df.filled <- subset(news.count.filled, fonte == this.fonte & cnpj == ts.cnpj)
       news.ts.filled <- zoo(news.df.filled$num_noticias, news.df.filled$data_noticia)
   
-      rm(price.ts.tmp, news.df, news.df.filled)
+      # Remove the useless objects
+      rm(news.df.filled)
       
-      # FILLED NEWS and FILLED PRICE (TS CORRELATION)
+      # CALCULTE THE Pearson CORRELATION between the FILLED NEWS and FILLED PRICE
       # Find the intersection between the filled TSs and Filter them
       intersect.ts.filled <- merge.zoo(news.ts.filled, price.ts.filled, all = F)
       
-      # Calculate the Filled Correlation
+      # Calculate the Correlation
       ts.correlation.filled <- cor(as.vector(intersect.ts.filled[, "news.ts.filled"]), 
                                    as.vector(intersect.ts.filled[, "price.ts.filled"]), 
                                    method = "pearson")
   
+      # CALCULTE THE Pearson CORRELATION between the FILLED NEWS and FILLED PRICE (solavanco dates)
       # Find the solavancos of the Filled Price TS
       intersect.news.ts <- intersect.ts.filled[, "news.ts.filled"]
       intersect.price.ts <- intersect.ts.filled[, "price.ts.filled"]
       
       solavancos <- LocalBaseline(intersect.price.ts, window.size)$is.burst
       
-      # Calculate the Pearson Correlation to the non-Solavanco dates (only)
-      
+      # Calculate the Correlation
       solavanco.corr <- cor(as.vector(intersect.news.ts)[c(F, solavancos)], 
                             as.vector(intersect.price.ts)[c(F, solavancos)], 
                             method = "pearson")
